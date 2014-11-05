87cebd388f7e11d031ea885f69fa9fc12d8adf0e
Emily Bamsey <em_bamsey@laurentian.ca>
2014-11-05 10:41:16 -0500

    refdesk.py file with days update
    Forgot to include this file in the initial commit for listing
    days.

--- refdesk.py
+++ refdesk.py
@@ -5,11 +5,13 @@ from os.path import abspath, dirname
 import datetime
 import psycopg2
 import sqlite3
+# urllib/urllib2 to make JSON available
+import urllib, urllib2
 
 # Database connection info
 DB_NAME = 'refstats'
-DB_HOST = 'localhost'
-DB_USER = 'refstats'
+#DB_HOST = 'localhost'
+#DB_USER = 'refstats'
 
 app = Flask(__name__)
 app.root_path = abspath(dirname(__file__))
@@ -34,10 +36,12 @@ def get_db():
     database, but then you would have to set up .pgpass or add a
     password parameter, so let's keep it simple.
     """
-    return psycopg2.connect(
-        database=DB_NAME,
-        user=DB_USER
-    )
+    try:
+        return psycopg2.connect(
+            database=DB_NAME
+        )
+    except Exception, e:
+        print(e)
 
 @app.route('/refdesk-stats', methods=['GET', 'POST'])
 def submit():
@@ -80,5 +84,59 @@ def show_stat_form():
     "Show the pretty form for the user"
     return render_template('stat_form.html', today=((datetime.datetime.now() + datetime.timedelta(hours=-2)).date().isoformat()))
 
+def get_stats(): 
+    "Get the stats from the database"
+    try:
+        dbase = get_db()
+        cur = dbase.cursor()
+        cur.execute('SELECT DISTINCT refdate FROM refstats')
+        dates = [dict(refdate=row[0]) for row in cur.fetchall()]
+        # dates = ('2004-10-01', '2014-10-31')
+        if dbase.closed:
+            return "I was closed!"
+        dbase.commit()
+        dbase.close()
+        return dates
+    except Exception, e:
+        print(e)
+
+def get_csv():
+    "Get the data in CSV format"
+    filename = flask.request.args.get('filename', '')
+    try:  
+        data = get_db()
+        cur = date.cursor()
+        csv = cur.execute('COPY(SELECT refdate, refstat, refcount FROM refstats WHERE refdate=%s) TO STDOUT WITH csv', (filename))
+        data.commit()
+        data.close()
+        return csv
+    except Exception, e:
+        print(e)
+
+@app.route('/showRefdesk-stats', methods=['GET'])
+def show_stats():
+    "Lets try to get all dates with data input"
+    try:
+        dates = get_stats()
+        return render_template('show_stats.html', dates=dates)
+    except:
+        return abort(500)
+
+@app.route('/download/<filename>')
+def download_file():
+    try:
+        filename = flask.request.args.get('filename', '')
+        "Try and create some JSON -> most likely won't work, I have no idea what I'm doing with JSON"
+        jsonu = u"%s/download/%s" % (config.JSON_HOST, urllib.quote_plus(filename.encode("uft-8")))
+        "Download the CSV dump of the selected day"
+        csv = get_csv()
+        response = make_respose(csv)
+        response_header = "attachment; fname=" + filename + ".csv"
+        response.headers["Content-Disposition"] = response_header
+        return response
+    except:
+        return abort(500)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5555)
+    app.run(debug=True, host="0.0.0.0", port=6666)
